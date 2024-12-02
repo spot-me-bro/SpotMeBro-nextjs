05272fe66468513453c4cc6a27847a343e88eb7d
ashtonaparra <aparra.ashton@gmail.com>
2024-12-02 10:34:13 -1000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,15 @@ const Home = () => (
         <p>
           Empowering students to stay consistent, motivated, and achieve their fitness goals together.
         </p>
+        <p>
+          SpotMeBro is a platform designed to help students find gym partners based on their fitness level,
+          experience, and goals.Whether you’re a beginner looking for guidance or an advanced gym-goer looking
+          for a partner to push you to your limits, SpotMeBro aims to make your workout experience more engaging
+          and productive. SpotMeBro is a platform designed to help students find gym partners based on their fitness
+          level, experience, and goals. Whether you’re a beginner looking for guidance or an advanced gym-goer looking
+          for a partner to push you to your limits, SpotMeBro aims to make your workout experience more engaging
+          and productive.
+        </p>
       </div>
     </Container>
   </main>
