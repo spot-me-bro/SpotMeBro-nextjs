91f6c08126573934f36e6711af9ad8976dc23cde
CoenBracilano <cpbswims@gmail.com>
2024-12-03 13:01:54 -1000

    Trying to fix vercell issues, sorry for all the commits

--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -6,21 +6,21 @@ import { prisma } from '@/lib/prisma';
 import EditProfileForm from '@/components/EditProfileForm';
 import authOptions from '@/lib/authOptions';
 
-export default async function EditProfilePage({ params }: { params: { id: string | string[] } }) {
+export default async function EditProfilePage({ params }: { params: { id: string } }) {
   // Protect the page, only logged in users can access it.
   const session = await getServerSession(authOptions);
   loggedInProtectedPage(
     session as {
       user: { email: string; id: string; randomKey: string };
-      // eslint-disable-next-line @typescript-eslint/comma-dangle
     } | null,
   );
-  const id = Number(Array.isArray(params?.id) ? params?.id[0] : params?.id);
-  // console.log(id);
+
+  const id = Number(params.id);
+
   const profile: Profile | null = await prisma.profile.findUnique({
     where: { id },
   });
-  // console.log(contact);
+
   if (!profile) {
     return notFound();
   }
