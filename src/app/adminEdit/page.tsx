9bbc59fd05894535af4d36661ab636e63a49ce2b
CoenBracilano <cpbswims@gmail.com>
2024-12-10 10:30:08 -1000

    Added a helper component to avoid using dynamic routing

--- src/app/adminEdit/page.tsx
+++ src/app/adminEdit/page.tsx
@@ -1,43 +1,16 @@
-import EditProfileForm from '@/components/EditProfileForm';
-import authOptions from '@/lib/authOptions';
 import { prisma } from '@/lib/prisma';
-import { getServerSession } from 'next-auth';
-import { notFound } from 'next/navigation';
+import EditProfileFormAdmin from '@/components/EditProfileForm-Admin';
 
-export default async function EditUserProfilePage({
-  searchParams,
-}: {
-  searchParams: { id?: string };
-}): Promise<JSX.Element> {
-  const profileId = Number(searchParams?.id);
-
-  if (!profileId || Number.isNaN(profileId)) {
-    return notFound();
+export default async function AdminEditPage(): Promise<JSX.Element> {
+  const profiles = await prisma.profile.findMany();
+  // Get all profiles
+  if (!profiles) {
+    throw new Error('Failed to fetch profiles');
   }
 
-  const session = await getServerSession(authOptions);
-
-  if (!session || !session.user?.email) {
-    console.log('failed to get id at all');
-    return notFound();
-  }
-
-  try {
-    const profile = await prisma.profile.findUnique({
-      where: { id: profileId },
-    });
-
-    if (!profile) {
-      return notFound();
-    }
-
-    return (
-      <main>
-        <EditProfileForm profile={profile} />
-      </main>
-    );
-  } catch (error) {
-    console.error('Failed to fetch profile:', error);
-    return notFound();
-  }
+  return (
+    <main>
+      <EditProfileFormAdmin profiles={profiles} />
+    </main>
+  );
 }
