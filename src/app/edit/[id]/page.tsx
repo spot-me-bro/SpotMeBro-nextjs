142d383c0b79faf67b609c46d35f3f14c275f585
CoenBracilano <cpbswims@gmail.com>
2024-12-09 20:01:31 -1000

    Replaced edit/[id] page.tsx file with the template version to try and isolatte the issue.

--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -1,37 +1,62 @@
 import { getServerSession } from 'next-auth';
-import { notFound } from 'next/navigation';
-import { Profile } from '@prisma/client';
-import { loggedInProtectedPage } from '@/lib/page-protection';
-import { prisma } from '@/lib/prisma';
-import EditProfileForm from '@/components/EditProfileForm';
 import authOptions from '@/lib/authOptions';
+import { loggedInProtectedPage } from '@/lib/page-protection';
 
-interface PageProps {
-  params: { id: string };
-}
-
-export default async function EditProfilePage({ params }: PageProps) {
+export default async function EditStuffPage({ params }: { params: { id: string | string[] } }) {
   // Protect the page, only logged in users can access it.
   const session = await getServerSession(authOptions);
   loggedInProtectedPage(
     session as {
       user: { email: string; id: string; randomKey: string };
+      // eslint-disable-next-line @typescript-eslint/comma-dangle
     } | null,
   );
-
-  const id = Number(params.id);
-
-  const profile: Profile | null = await prisma.profile.findUnique({
-    where: { id },
-  });
-
-  if (!profile) {
-    return notFound();
-  }
+  const id = Number(Array.isArray(params?.id) ? params?.id[0] : params?.id);
+  console.log(id);
 
   return (
     <main>
-      <EditProfileForm profile={profile} />
+      <h1>
+        Testing to see if anythign is working
+      </h1>
     </main>
   );
 }
+
+// import { getServerSession } from 'next-auth';
+// import { notFound } from 'next/navigation';
+// import { Profile } from '@prisma/client';
+// import { loggedInProtectedPage } from '@/lib/page-protection';
+// import { prisma } from '@/lib/prisma';
+// import EditProfileForm from '@/components/EditProfileForm';
+// import authOptions from '@/lib/authOptions';
+
+// interface PageProps {
+//   params: { id: string };
+// }
+
+// export default async function EditProfilePage({ params }: PageProps) {
+//   // Protect the page, only logged in users can access it.
+//   const session = await getServerSession(authOptions);
+//   loggedInProtectedPage(
+//     session as {
+//       user: { email: string; id: string; randomKey: string };
+//     } | null,
+//   );
+
+//   const id = Number(params.id);
+
+//   const profile: Profile | null = await prisma.profile.findUnique({
+//     where: { id },
+//   });
+
+//   if (!profile) {
+//     return notFound();
+//   }
+
+//   return (
+//     <main>
+//       <EditProfileForm profile={profile} />
+//     </main>
+//   );
+// }
